cb1f2aca580b1732a97b43e279383255b515f343
manoel freitas <manoel.josefneto@gmail.com>
2017-02-07 16:06:15 -0300

    Up:Atualizando lib
    Retirei o codigo que criei para usar uma biblioteca

--- utils/checksum.js
+++ utils/checksum.js
@@ -1,23 +1,17 @@
 "use strict";
 
-const crypto = require('crypto'),
-      fs     = require('fs');
-
-function checksum(str, algorithm, encoding) {
-    return crypto.createHash(algorithm || 'md5').update(str, 'utf8').digest(encoding || 'hex')
-}
+const checksum = require('checksum');
 
 
 class Checksum {
 
     static generateCheckSum(file) {
-        var bufferFile = '';
-        fs.readFile(file,function (err,data) {
-            if(err) console.error(err);
-            bufferFile = data;
-        });;
-
-        return checksum(bufferFile, 'sha1');
+        return checksum(file,function (err,sum) {
+            if (err) {
+                throw new Error(err);
+            }
+            return sum;
+        })
     }
     valideChecksum(checksum) {
         return this.checksum === checksum;
