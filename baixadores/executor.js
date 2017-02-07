f697fd4b37d93dc18b5005185fe2d642c882aa4e
manoel freitas <manoel.josefneto@gmail.com>
2017-02-07 15:57:39 -0300

    Feat: Refatorando toda a classe
    A classe não seguia o fluxo da aplicação e teve que ser refeito varios pontos

--- baixadores/executor.js
+++ baixadores/executor.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const Checksum      = require('../utils/checksum'),
-      FileUtils     = require('../utils/fileutils'),
+const FileUtils     = require('../utils/fileutils'),
       ExtractUtils  = require('../utils/extract'),
       Service       = require('./service'),
       copy          = require('copy'),
@@ -19,24 +18,12 @@ class Executor {
 
     extrairArquivo(file) {
         let Executor = this;
-        ExtractUtils.extractFile(file, this.baixador.workingDir).then((files) => {
-            this.logger.info(`${this.file.get()} extraido com sucesso`);
-            let Executor = this;
-            Executor.moveFile(Executor, files).then((filesMoved) => {
-                Executor.logger.log('info', `Arquivos Movidos para ${Executor.configuracaoBaixador.path_destino}`);
-                FileUtils.removeFile(this.baixador.workingDir);
-            }).catch((err) => {
-                Executor.logger.log('error', 'Erro ao mover arquivos para backup: ' + err);
-                Executor.service.saveErrorLog(err);
-            })
-        }).catch(err => {
-            Executor.logger.error(`Falha ao extrair arquivo: ${err}`);
-            service.saveErrorLog(err);
-        });
+        return ExtractUtils.extractFile(file, this.baixador.workingDir);
     }
-    moveFile(Executor, files) {
+    moveFile(files) {
+        let Executor = this;
         return new Promise(function(resolve, reject) {
-            copy.each(files, Executor.configuracaoBaixador.path_destino, function(err, files) {
+            copy.each(files, Executor.configuracaoBaixador.pathDestino, function(err, files) {
                 if (err) {
                     return reject(err);
                 }
