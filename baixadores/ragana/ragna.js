0b31f16f3656cf7e0a166da237b7811718d3e89c
manoel freitas <manoel.josefneto@gmail.com>
2017-02-07 16:01:28 -0300

    Feat: Refatorando toda a calsse
    A classe não seguia o fluxo da aplicação entao teve que ser refatorada em varios pontos

--- baixadores/ragana/ragna.js
+++ baixadores/ragana/ragna.js
@@ -1,19 +1,21 @@
 "use strict";
 
-const path = require('path'),
-      Executor = require('../executor'),
-      FileUtils = require('../../utils/fileutils'),
-      BaixadorRagna = require('./ragana-baixador');
-
+const Checksum = require('../../utils/checksum'),
+    path = require('path'),
+    Executor = require('../executor'),
+    FileUtils = require('../../utils/fileutils'),
+    BaixadorRagna = require('./ragana-baixador');
 
 class Ragana extends Executor {
     constructor(app, tempdir, configuracaoBaixador) {
         super(app, configuracaoBaixador);
         const baixador = new BaixadorRagna(true, app, configuracaoBaixador);
-        baixador.setWorkingDir(`temp${path.sep}${tempdir}`);
+        baixador.setWorkingDir(`Arquivos${path.sep}${tempdir}`);
         FileUtils.mkDir(baixador.workingDir);
         this.file = configuracaoBaixador.pathDestino;
         this.baixador = baixador;
+        this.cfg = configuracaoBaixador;
+        this.models = app.db.models;
     }
 
     logica() {
@@ -28,9 +30,26 @@ class Ragana extends Executor {
 
             Ragana.baixador.login().walkToOperacoes().walkToVendas().fillFormVendas(date).downloadFiles().then(() => {
                 Ragana.logger.info(`Finalizado download de arquivos da Ragana em ${now}`);
-                Ragana.extrairArquivo(`${Ragana.file.get()}`);
-                return resolve(Ragana.service.saveSucessLog());
+                if (Ragana.file.exists) {
+                    throw new Error(`Arquivo já foi Baixado, verifique os arquivos em ${Ragana.cfg.pathDestino}`)
+                }
+                if (Ragana.file.name.endsWith('.zip')) {
+                    Ragana.extrairArquivo(`${Ragana.file.get()}`).then((files) => {
+                        Ragana.logger.info(`${Ragana.file.get()} extraido com sucesso`);
+                        Ragana.moveFile(files).then((filesMoved) => {
+                            Ragana.logger.log('info', `Arquivos Movidos para ${Ragana.cfg.pathDestino}`);
+                        }).catch((err) => {
+                            Ragana.logger.log('error', 'Erro ao mover arquivos para backup: ' + err);
+                            Ragana.service.saveErrorLog(err);
+                        })
+                    }).catch(err => {
+                        Ragana.logger.error(`Falha ao extrair arquivo: ${err}`);
+                        Ragana.service.saveErrorLog(err);
+                    });
+                }
+                return resolve(Ragana.service.saveSucessLog(Ragana.file));
             }).catch(function(error) {
+
                 Ragana.logger.error(`Finalizado download de arquivos da Ragana em ${now}`);
 
                 Ragana.service.saveErrorLog({message: `Erro ao finalizar crawler da raganar: ${error}`});
@@ -45,16 +64,33 @@ class Ragana extends Executor {
         this.baixador.on('download', function(downloadItem) {
             Ragana.logger.info('Iniciando download de ' + downloadItem.filename);
             Ragana.logger.info('Download em ' + Ragana.baixador.workingDir);
-
             var file = `${Ragana.baixador.workingDir}${path.sep}${downloadItem.filename}`;
+
             Ragana.baixador.download(file, downloadItem);
+            let checksumFile = Checksum.generateCheckSum(file);
             Ragana.file = {
                 path: Ragana.baixador.workingDir,
                 name: downloadItem.filename,
                 get: () => {
                     return file
-                }
+                },
+                checksum: checksumFile,
+                exists: false
             };
+            Ragana.models.log_baixador_sucesso.find({
+                where: {
+                    checksum: checksumFile
+                }
+            }).then((log) => {
+                if(log){
+                    FileUtils.removeFile(file);
+                    Ragana.file.exists = true;
+                }
+            }, (err) => {
+                Ragana.logger.error(`Falha ao localizar arquivo com o checksun ${checksumFile} \nErro:${err}`);
+                Ragana.service.saveErrorLog({message:`Falha ao localizar arquivo com o checksun ${checksumFile} \nErro:${err}`})
+            })
+
         }, function(state, downloadItem) {
             switch (state) {
                 case "interrupted":
